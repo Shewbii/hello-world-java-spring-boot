23a3abb011de8b1780dcd2b91b64b33a798aaf6a
Alexis JEANNE <contact@alexisjeanne.fr>
2022-07-17 18:42:13 +0200

    first commit

---
+++ src/main/java/fr/shewbii/helloworldjavaspringboot/HelloWorldJavaSpringBootApplication.java
@@ -0,0 +1,13 @@
+package fr.shewbii.helloworldjavaspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWorldJavaSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWorldJavaSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/shewbii/helloworldjavaspringboot/HelloWorldJavaSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.shewbii.helloworldjavaspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWorldJavaSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
